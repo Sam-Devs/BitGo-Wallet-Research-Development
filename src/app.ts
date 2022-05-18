f21fc88846566b0eb9203c72d6c96dbc42495c5a
Sam Ayorinde <ayorinde2@gmail.com>
2022-05-18 18:15:10 +0100

    app listner

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import express, { Response, Request } from "express";
+const app  = express();
+import cors from "cors";
+import dotenv from "dotenv";
+// import router from "";
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+dotenv.config();
+
+app.use(cors());
+app.use((req: Request, res: Response, next: any) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Content-type", "application/json");
+    res.header("Access-Control-Allow-Methods", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
+
+app.get("/",async (req: Request, res: Response) => {
+    res.send({status: 200, message: "Server is up and running"})
+})
+
+const port: string | number = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    return console.log(`Server is running on port ${port}`);
+    
+})
