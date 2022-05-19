f43fdfded4128b8e454e910a5298934e5635d3cb
Sam Ayorinde <ayorinde2@gmail.com>
2022-05-19 15:26:16 +0100

    update wallet

--- src/handlers/Wallet.ts
+++ src/handlers/Wallet.ts
@@ -27,3 +27,28 @@ export const createWallet = async (req: Request, res: Response) => {
 }
 
 // Update wallet address
+export const updateWallet = async (req: Request, res: Response) => {
+    const { coin, walletId, address } = req.query;
+    const { label, passphrase } = req.body;
+
+    const params = {
+        label,
+        passphrase
+    }
+
+    await axios
+    .put(`${BASE_URL}/${coin}/wallet/${walletId}`, params, {
+        headers: {
+            Authorization: `Bearer ${process.env.ACCESS_TOKEN}`
+        }
+    })
+    .then((wallet: any) => {
+        return res.send({ status: 200, data: wallet.data })
+    }).catch((err: any) => {
+        console.log(err);
+        return res.send({ status: 400, message: err })
+    });
+}
+
+// Delete wallet
+export 
