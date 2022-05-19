139144b697ea77f2b62eb9ca59b07ed159788b5c
Sam Ayorinde <ayorinde2@gmail.com>
2022-05-19 16:08:46 +0100

    routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,24 @@
 import express from "express";
-import { createAddress, allAddresses, updateAddress, singleAddress } from "../handlers/Address";
+import {
+  createAddress,
+  allAddresses,
+  updateAddress,
+  singleAddress,
+} from "../handlers/Address";
 import { addressBalance, walletBalance } from "../handlers/Balances";
-import { transfer, pendingTransaction, allTransfers, singleTransfer } from "../handlers/Transfer";
-import { createWallet, updateWallet, deleteWallet, getAllWallets, getWallet } from "../handlers/Wallet";
+import {
+  transfer,
+  pendingTransaction,
+  allTransfers,
+  singleTransfer,
+} from "../handlers/Transfer";
+import {
+  createWallet,
+  updateWallet,
+  deleteWallet,
+  getAllWallets,
+  getWallet,
+} from "../handlers/Wallet";
 
 const router = express.Router();
 
@@ -11,3 +27,10 @@ router.post("/wallet", createWallet);
 router.get("/wallets", getAllWallets);
 router.get("/wallet", getWallet);
 router.put("/wallet", updateWallet);
+router.delete("wallet", deleteWallet);
+
+// Address
+router.post("/address", createAddress);
+router.put("/address", updateAddress);
+router.get("/addresses", allAddresses);
+router.get("/address", singleAddress);
