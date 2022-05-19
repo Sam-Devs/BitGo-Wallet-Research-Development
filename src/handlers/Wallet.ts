2f7f359e8c523bd09aa8bfe2e3833d03c57815dc
Sam Ayorinde <ayorinde2@gmail.com>
2022-05-19 15:10:27 +0100

    create wallet

--- src/handlers/Wallet.ts
+++ src/handlers/Wallet.ts
@@ -5,3 +5,25 @@ import bitgo from "../config/bitgo";
 
 
 // Create Wallet
+export const createWallet = async (req: Request, res: Response) => {
+    const { coin, label, passphrase } = req.body;
+
+    const params = {
+        label,
+        passphrase
+    }
+
+    bitgo
+    .coin(coin)
+    .wallets()
+    .generateWallet(params)
+    .then((wallet: any) => {
+        console.log({wallet});
+        res.send({ status: 200, data: wallet })
+    }).catch((err: any) => {
+        console.log(err);
+        return res.send(err)
+    });
+}
+
+// Update wallet address
