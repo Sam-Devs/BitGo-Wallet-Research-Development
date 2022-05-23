360f749fc654d58759dc6a4117ae4c1ede55d383
Sam Ayorinde <ayorinde2@gmail.com>
2022-05-23 23:19:22 +0100

    webhooks updated

--- src/webhooks/webhooks.ts
+++ src/webhooks/webhooks.ts
@@ -1,3 +1,28 @@
 import bitgo from "../config/bitgo";
 import { Request, Response} from "express"
-// import 
+import { IWebhook } from "../interfaces";
+
+// Create a webhook 
+
+export const addWalletWebhook = (req: Request, res: Response) => {
+    const { coin, walletId }: any = req.query;
+    const {callbacklurl, type }: any = req.query;
+
+    bitgo
+    .coin(coin as any)
+    .wallets()
+    .get({ id: walletId })
+    .then((wallet: any) => {
+        return wallet.addWebhook({
+            url: callbacklurl,
+            type: type
+        })
+    })
+    .then((webhook: IWebhook) => {
+        console.log(webhook);
+        return res.send({ status: 200, data: webhook })
+    }).catch((err: any) => {
+        console.log(err);
+        return res.send(err)
+    });
+}
